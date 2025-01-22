8b860babdacf57696702c506fb3bc2fb56299a69
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-01-22 20:58:58 -0300

    Get game by id

--- src/main/java/com/pedromartinsl/dslist/services/GameService.java
+++ src/main/java/com/pedromartinsl/dslist/services/GameService.java
@@ -4,16 +4,26 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.pedromartinsl.dslist.dto.GameDTO;
 import com.pedromartinsl.dslist.dto.GameMinDTO;
 import com.pedromartinsl.dslist.entities.Game;
 import com.pedromartinsl.dslist.repositories.GameRepository;
+
 
 @Service
 public class GameService {
     
     @Autowired
 	private GameRepository gameRepository;
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
+	}
 	
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
